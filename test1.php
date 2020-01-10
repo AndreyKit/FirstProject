58fab54e072ee3238954d57c5240e043a2aa6e27
AndreyKit <59205429+AndreyKit@users.noreply.github.com>
2020-01-10 19:51:21 +0300

    Update test1.php

--- test1.php
+++ test1.php
@@ -14,17 +14,67 @@
 
 
 
-     <?php
+     <!-- <?php
     $a = 5;
-    if ($a == 5): ?>
-       A  is equal 5
-    <?php endif; ?> 
-    <?php 
+    if ($a == 5) : ?>
+        A is equal 5
+    <?php endif; ?>
+    <?php
     $a = 5;
     if ($a == 5) { ?>
-        A  is equal 5
+        A is equal 5
     <?php }; ?>
+    <?php
+
+    for ($i = 1; $i <= 10; $i++) {
+        echo "$i<br >";
+    }
+    ?>
+    <?php
+
+    for ($i = 1;; $i++) {
+
+        if ($i > 10) {
+            break;
+        }
+        echo "$i<br>";
+    }
+
+    ?>
+    <?php
+
+    $i = 1;
+    for (;;) {
+        if ($i > 10) {
+            break;
+        }
+        echo $i;
+        $i++;
+    }
+
+
+    ?> 
+    <?php
+    $s = 0;
     
+    for ($i = 1; $i <= 10; $i++) {
+        $s = $s + $i;
+       
+    }
+     echo $s
+    ?> -->
+    <?php 
+$arr = array("null","one","two");
+
+foreach ($arr as $key => $value){
+    echo "key: $key; valye: $value<br/>\n";
+}
+$arr = array("one","two","three");
+
+foreach ($arr as $value){
+    echo "Value: $value<br/>\n";
+}
+    ?>
 </body>
 
 </html>
