1826ddcbff0a081ba3c54938e3c9e9e14779cf9f
AndreyKit <59205429+AndreyKit@users.noreply.github.com>
2020-01-14 19:38:18 +0300

    new

--- test2.php
+++ test2.php
@@ -1,15 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
-    <?php 
+   <!-- <?php
     $makefoo = true;
-    $bar();
-    ?>
+    bar();
+    if ($makefoo) {
+        function foo()
+
+        {
+            echo "I don't exist";
+        }
+    }
+if ($makefoo) foo();
+ function bar () {
+    echo" I exist emmediatly.\n";
+}
+    ?> -->
+    
 </body>
+
 </html>
