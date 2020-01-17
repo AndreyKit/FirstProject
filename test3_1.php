5001729b348b355526d5752b9aca9fde9cde8951
AndreyKit <59205429+AndreyKit@users.noreply.github.com>
2020-01-17 18:59:35 +0300

    Update test3_1.php

---
+++ test3_1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $array = array(10, 21, 32, 44, 65, 26);
+    function numb($array){
+        for($i=1; $i <= count($array); $i++) {
+            if ($array[$i] > $max){
+                $max = $array[$i];
+            }
+        }
+             return $max
+    }
+    echo numb($array)
+    ?>
+</body>
+</html>
