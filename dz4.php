8bd670bb70f39fcfe32380c9a381fb9940061e8e
AndreyKit <59205429+AndreyKit@users.noreply.github.com>
2020-01-24 19:38:49 +0300

    Update dz4.php

---
+++ dz4.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+  
+<?php
+
+$f=fopen("stat.txt","a+");
+flock($f,LOCK_EX);
+$count=fread($f,100);
+@$count++;
+ftruncate($f,0);
+fwrite($f,$count);
+fflush($f);
+flock($f,LOCK_UN);
+fclose($f);
+
+?>
+   
+</body>
+</html>
