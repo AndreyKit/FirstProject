c483fd1539e1ca7887d165b8b7c895a842ae64ca
AndreyKit <59205429+AndreyKit@users.noreply.github.com>
2020-01-31 18:52:40 +0300

    2

---
+++ dz6.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $i=$_POST['s'];
+    $arr = explode(';',file_get_contents('opros.csv '));
+    $arr[$i]++;
+    file_put_contents('opros.csv', implode(";",$arr));
+    foreach($arr as $key=>$value){
+        echo "$key - $value";
+    }
+    ?>
+</body>
+</html>
