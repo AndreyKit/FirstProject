5cc7b6301067cf2355989878c81e3ab370a83928
AndreyKit <59205429+AndreyKit@users.noreply.github.com>
2020-01-31 19:36:23 +0300

    explote

--- new form/new1.php
+++ new form/new1.php
@@ -7,14 +7,23 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="" method="POST">
     <?php 
     $arr = file("new2.csv");
 ?>
 <h1><?=$arr[0]?></h1>
 
-<? 
-for()
-?>
+<?
 
+
+
+for ($i = 1; $i < count($arr);$i++){
+$buf = explode("-",$arr[$i])[0];
+echo "<label><input type='radio' name='vot' value='$i'>  $buf</label><br>\n";
+}
+
+?>
+<input type="submit" value="OK">
+</form>
 </body>
 </html>
