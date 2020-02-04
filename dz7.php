4df6704e3724ecff7c9570c297607da99015977b
AndreyKit <59205429+AndreyKit@users.noreply.github.com>
2020-02-04 19:21:09 +0300

    3245

---
+++ dz7.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .list {
+            background-color: #E6E4E4;
+            width: 250px;
+            height: 170px;
+            padding: 15px;
+            margin: auto;
+        }
+        .listdown{
+            background-color: #fff;
+            height:200px;
+            padding: 15px;
+        }
+        .list p{
+            text-align: justify;
+            font-size: 22px;
+        }
+        div{
+            border-radius: 1em;
+            box-shadow: #2c96e3;
+        }
+    </style>
+</head>
+<form action="" method="get" target="_blank">
+    <div class="list">
+        <p>What is your question?</p>
+        <div class="listdown">
+            <input type="hiden" name="vote" value="1">69% Dont have one <br>
+            <input type="hiden" name="vote" value="2">22% Why? <br>
+            <input type="hiden" name="vote" value="3">0% When <br>
+            <input type="hiden" name="vote" value="4">9% Where <br>
+        </div>
+    </div>
+</form>
+<body>
+
+</body>
+
+</html>
---
+++ dz7_1.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .list {
+            background-color: #E6E4E4;
+            width: 250px;
+            height: 360px;
+            padding: 15px;
+            margin: auto;
+        }
+
+        .listdown {
+            background-color: #fff;
+            font-size: 15px;
+            padding: 15px;
+        }
+
+        .list p {
+            text-align: justify;
+            font-size: 22px;
+        }
+
+        div {
+            border-radius: 1em;
+            box-shadow: 0 0 5;
+            padding: 5px;
+        }
+
+        input {
+            border-radius: 1em;
+            border-color: #2c96e3;
+
+        }
+
+        #noname {
+            width: 100px;
+            height: 30px;
+            color: black;
+            margin: 10px 5px;
+            text-align: center;
+            font-size: 35px;
+            
+        }
+    </style>
+</head>
+<form action="dz7.php" method="get" target="_blank">
+    <div class="list">
+        <p>What subject would you like to lern</p>
+        <div class="lixtdown">
+            <input type="Radio" name="vote" value="1" checked> Html & CSS
+            <hr>
+            <input type="Radio" name="vote" value="2"> Java Script
+            <hr>
+            <input type="Radio" name="vote" value="3"> Framework
+            <hr>
+            <input type="Radio" name="vote" value="4"> Ruby
+            <hr>
+            <input type="Radio" name="vote" value="5"> PHP
+            <hr>
+            <input type="Radio" name="vote" value="6"> my SQL
+            <hr>
+
+
+        </div>
+        <input type="Sumbit" value="VOTE" id="noname">
+    </div>
+
+
+</form>
+
+<body>
+
+</body>
+
+</html>
